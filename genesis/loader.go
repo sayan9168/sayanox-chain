8277dc7b8b5d8f7293efdaea407f28e45ba62757
Sayan <sayan9168@users.noreply.github.com>
2026-07-12 17:45:33 +0530

    Add genesis configuration and loader

---
+++ genesis/loader.go
@@ -0,0 +1,31 @@
+package genesis
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Genesis struct {
+	ChainID      string `json:"chain_id"`
+	NetworkName  string `json:"network_name"`
+	Version      string `json:"version"`
+	GenesisTime  string `json:"genesis_time"`
+	InitialSupply uint64 `json:"initial_supply"`
+	Symbol       string `json:"symbol"`
+	Decimals     uint8  `json:"decimals"`
+	Validators   []string `json:"validators"`
+}
+
+func LoadGenesis(path string) (*Genesis, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var g Genesis
+	if err := json.Unmarshal(data, &g); err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+}
